b7e6b6110c7b908874803e8d9457acef90605db6
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2023-11-01 11:27:02 -0300

    Add files via upload

---
+++ sum_Array.py
@@ -0,0 +1,17 @@
+# Sum solution
+def sum_array(a):
+    if a:
+        return sum(a)
+    else:
+        return 0
+
+# For solution
+def sum_aray(b):
+    n = 0
+    for i in b:
+        n += i
+    return n
+
+
+print(sum_aray([1, 2, 3]), 6)
+
