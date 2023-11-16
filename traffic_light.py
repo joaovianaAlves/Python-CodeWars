039183ba5e477a3b65c8d39ee7a95bbf93dffb73
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2023-11-16 19:46:21 -0300

    Create traffic_light.py

---
+++ traffic_light.py
@@ -0,0 +1,9 @@
+def update_light(current):
+#     if current == 'green':
+#         return 'yellow'
+#     elif current == 'yellow':
+#         return 'red'
+#     else:
+#         return 'green'
+    dicionario = {'green': 'yellow','yellow': 'red', 'red': 'green'}
+    return dicionario.get(current)
