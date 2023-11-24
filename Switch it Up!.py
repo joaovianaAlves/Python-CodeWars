c55c2d6a24972fbb93ab712e98afffddd50cd1d1
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2023-11-24 09:39:25 -0300

    Create Switch it Up!.py

---
+++ Switch it Up!.py
@@ -0,0 +1,20 @@
+# When provided with a number between 0-9, return it in words.
+
+# Input :: 1
+
+# Output :: "One".
+
+def switch_it_up(number):
+    number_words = {
+        0: 'Zero',
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine'
+    }
+    return number_words.get(number)
